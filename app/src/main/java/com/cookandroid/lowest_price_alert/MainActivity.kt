e78a477b6ea092ddefe804a22ee3309b11ce0daa
yejinLee <yejin9989@naver.com>
2021-11-11 21:13:25 +0900

    make function for email login

--- app/src/main/java/com/cookandroid/lowest_price_alert/MainActivity.kt
+++ app/src/main/java/com/cookandroid/lowest_price_alert/MainActivity.kt
@@ -5,41 +5,59 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    // variables for login
     lateinit var emailEditText : EditText
     lateinit var passwordEditText: EditText
     lateinit var loginBtn : Button
-    lateinit var googleLoginBtn : Button
+    lateinit var google_sign_in_button : SignInButton
+
+    // declare nullable object for Firebase auth
+    var auth: FirebaseAuth? = null
+
+    // declare nullable object for google login
+    var googleSignInClient : GoogleSignInClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //nullable한 FirebaseAuth 객체 선언
-        var auth: FirebaseAuth? = null
-        //auth 객체 초기화
-        auth = FirebaseAuth.getInstance()
-
         emailEditText = findViewById(R.id.emailEditText)
         passwordEditText = findViewById(R.id.passwordEditText)
         loginBtn = findViewById(R.id.loginBtn)
-        googleLoginBtn = findViewById(R.id.googleLoginBtn)
+        google_sign_in_button = findViewById(R.id.google_sign_in_button)
 
-        loginBtn.setOnClickListener{
-            auth?.signInWithEmailAndPassword(emailEditText.text.toString(),passwordEditText.text.toString())
-                ?.addOnCompleteListener{//통신 완료가 된 후 무슨일을 할지
-                        task->
-                    if(task.isSuccessful){
-                        //로그인 처리를 해주면 됨!
-                        Toast.makeText(this, "login", Toast.LENGTH_LONG).show()
-                    }
-                    else{
-                        // 오류가 난 경우!
-                        Toast.makeText(this,task.exception?.message, Toast.LENGTH_LONG).show()
-                    }
-                }
+        //auth 객체 초기화, 인스턴스 get
+        auth = FirebaseAuth.getInstance()
+
+        // login with email
+        loginBtn.setOnClickListener {
+            emailLogin()
         }
+    } // onCreate
+
+    fun emailLogin() {
+        auth?.signInWithEmailAndPassword(emailEditText.text.toString(),passwordEditText.text.toString())
+            ?.addOnCompleteListener{//통신 완료가 된 후 무슨일을 할지
+                    task->
+                if(task.isSuccessful){
+                    //로그인 처리를 해주면 됨!
+                    Toast.makeText(this, "login", Toast.LENGTH_LONG).show()
+                }
+                else{
+                    // 오류가 난 경우!
+                    Toast.makeText(this,task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        //email, password null인 경우 예외 처리 해주
+    }
+    fun googleLogin() {
+
     }
 }
