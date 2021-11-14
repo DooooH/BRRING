206b7d98d248905b7b72c00e11a6340acb700c81
dohkim <dh54kim54@gmail.com>
2021-11-14 16:18:38 +0900

    delete .idea

--- crawler/price.py
+++ crawler/price.py
@@ -1,8 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+import pyrebase
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}
 url = 'http://prod.danawa.com/info/?pcode=15253217&cate=12210596'
+# pcode : 상품코드, cate : category
+
+#Firebase database 인증 및 앱 초기화
+firebase_admin.initialize_app({
+    'databaseURL' : 'https://mobileappprogramming-3c71d-default-rtdb.firebaseio.com/'
+})
+
+ref = db.reference() #db 위치 지정
+ref.update({'반원' : '고슴도치'}) #해당 변수가 없으면 생성한다.
 
 response = requests.get(url, headers=headers)
 
