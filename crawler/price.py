252ec2e735ea3cb86e7d5fcb3a9d2f680eb91848
dohkim <dh54kim54@gmail.com>
2021-11-16 17:20:28 +0900

    크롤링 함수 반복

--- crawler/price.py
+++ crawler/price.py
@@ -1,16 +1,53 @@
 import requests
+import pytz
+import re
+import time
+from datetime import datetime
 from urllib.parse import urlparse, parse_qsl
 from bs4 import BeautifulSoup
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-from datetime import datetime
-import pytz
-import re
 
-tz = pytz.timezone('Asia/Seoul')
-cur_time = datetime.now(tz).strftime('%Y-%m-%d') # 년도-월-일 별로 가격
-print(f'Seoul time: {cur_time}')
+def crawl_update(url, headers, cur_time):
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # 가격
+        # 새로 가져온 가격이 더 낮을 때만 갱신
+        result = soup.find('em', class_='prc_c')
+        new_price = int(re.sub(r'[^0-9]', '', result.get_text()))
+        try :
+            prev = price_ref.get(cur_time)[0][cur_time]
+            if new_price < prev:
+                price_ref.update({cur_time: new_price})
+        except:
+            price_ref.update({cur_time: new_price})
+        print("가격: ", new_price)
+
+        # 최저가
+        try:
+            min_price = prod_ref.child('min_price').get()
+            # print(min_price)
+            if new_price < min_price:
+                prod_ref.update({'min_price': new_price})
+        except:
+            prod_ref.update({'min_price': new_price}) # 이 전 가격이 없을 때
+        print("최저가: ", prod_ref.child('min_price').get())
+
+        # 이미지
+        img = soup.find('div', class_='photo_w')
+        img_src = img.find('img')
+        prod_ref.update({'img': img_src['src']})
+        print(img_src['src'])
+
+        # 스펙
+        spec = soup.find('div', class_='spec_list')
+        prod_ref.update({'spec': spec.get_text().strip()})
+        print(spec.get_text().strip())
 
 # Firebase database 인증 및 앱 초기화
 cred = credentials.Certificate('key.json')
@@ -26,41 +63,9 @@ queries = dict(parse_qsl(urlparse(url).query))
 prod_ref = db.reference('product_list/' + queries['pcode']) # 상품코드별, 날짜별 위치 지정
 price_ref = prod_ref.child('price')
 
-response = requests.get(url, headers=headers)
-
-if response.status_code == 200:
-    html = response.text
-    soup = BeautifulSoup(html, 'html.parser')
-
-    # 가격
-    # 새로 가져온 가격이 더 낮을 때만 갱신
-    result = soup.find('em', class_='prc_c')
-    new_price = int(re.sub(r'[^0-9]', '', result.get_text()))
-    try :
-        prev = price_ref.get(cur_time)[0][cur_time]
-        if new_price < prev:
-            price_ref.update({cur_time: new_price})
-    except:
-        price_ref.update({cur_time: new_price})
-    print("가격: ", new_price)
-
-    # 최저가
-    try:
-        min_price = prod_ref.child('min_price').get()
-        # print(min_price)
-        if new_price < min_price:
-            prod_ref.update({'min_price': new_price})
-    except:
-        prod_ref.update({'min_price': new_price}) # 이 전 가격이 없을 때
-    print("최저가: ", prod_ref.child('min_price').get())
-
-    # 이미지
-    img = soup.find('div', class_='photo_w')
-    img_src = img.find('img')
-    prod_ref.update({'img': img_src['src']})
-    print(img_src['src'])
-
-    # 스펙
-    spec = soup.find('div', class_='spec_list')
-    prod_ref.update({'spec': spec.get_text().strip()})
-    print(spec.get_text().strip())
+while True:
+    tz = pytz.timezone('Asia/Seoul')
+    cur_time = datetime.now(tz).strftime('%Y-%m-%d')  # 년도-월-일 별로 가격
+    print(f'Seoul time: {cur_time}')
+    crawl_update(url, headers, cur_time)
+    time.sleep(600) # 10분
