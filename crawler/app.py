3ace8d404b5eca815b277869fdf3c0d2858f5bfe
dohkim <dh54kim54@gmail.com>
2021-11-30 17:38:45 +0900

    flask host 수정

--- crawler/app.py
+++ crawler/app.py
@@ -68,3 +68,10 @@ app = Flask(__name__)
 def get_result(keyword):
     url = 'http://search.danawa.com/dsearch.php?k1=' + keyword
     return jsonify(parse_search(url))
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
