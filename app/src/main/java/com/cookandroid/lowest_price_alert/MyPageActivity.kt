937a22d6a486220bd7dac4669458a7b00b63edef
이준하 <51983113+JunhaLee@users.noreply.github.com>
2021-12-02 03:49:32 +0900

    Update MyPageActivity.kt

--- app/src/main/java/com/cookandroid/lowest_price_alert/MyPageActivity.kt
+++ app/src/main/java/com/cookandroid/lowest_price_alert/MyPageActivity.kt
@@ -3,17 +3,24 @@ package com.cookandroid.lowest_price_alert
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils.centerCrop
 
 class MyPageActivity : AppCompatActivity() {
 
-    lateinit var zzim_list_Btn : Button
+    lateinit var zzim_list_Btn : ImageButton
     lateinit var notice_Btn : Button
     lateinit var userguide_Btn : Button
     
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mypage_activity)
+
+        var userImage = findViewById<ImageView>(R.id.userImage)
+        Glide.with(this).load(R.drawable.ipad).circleCrop().into(userImage)
 
         zzim_list_Btn = findViewById(R.id.zzim_list_Btn)
         notice_Btn = findViewById(R.id.notice_Btn)
